d62cc1f751509819342de62419548f89a1f206e0
Rohit Singh <rohitsinghpb4@gmail.com>
2024-06-20 13:58:49 +0530

    home page edited

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,13 @@
 function HomePage() {
-  return <h1>The Home Page</h1>;
+    return (
+        <div>
+            <h1>The Home Page</h1>;
+            <p>
+                Hey bros there is me Rohit Signh my first deployed web app on
+                which you are now interfacing
+            </p>
+        </div>
+    );
 }
 
 export default HomePage;
